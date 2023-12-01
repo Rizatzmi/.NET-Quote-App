97e7633245cad6508e26f40c868b62f0e62da9f2
Rizatzmi <Rizatzmi19@gmail.com>
2023-12-01 20:35:28 +0700

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace attempt_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<string> Quotes { get; set; }
+
+        public void OnGet()
+        {
+            Quotes = System.IO.File.ReadAllLines("quotes.txt").ToList();
+        }
+    }
+}
